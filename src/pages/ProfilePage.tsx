a42d30473552916822eac92f881e71e40a33988d
PatrikWalde <pw@patrikwalde.com>
2025-09-08 09:25:25 +0200

    Fix checkbox types with double negation
    - Use double negation for checkbox checked property
    - Convert boolean to proper checkbox type

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -296,7 +296,7 @@ const ProfilePage: React.FC = () => {
                 <label className="flex items-center">
                   <input
                     type="checkbox"
-                    checked={formData.showOnLeaderboard as any}
+                    checked={!!formData.showOnLeaderboard}
                     onChange={(e) => setFormData({ ...formData, showOnLeaderboard: e.target.checked })}
                     disabled={!isEditing}
                     className="h-4 w-4 text-primary-600 focus:ring-primary-500 border-gray-300 dark:border-gray-600 rounded disabled:opacity-50"
@@ -314,7 +314,7 @@ const ProfilePage: React.FC = () => {
                 <label className="flex items-center">
                   <input
                     type="checkbox"
-                    checked={formData.allowAnalytics as any}
+                    checked={!!formData.allowAnalytics}
                     onChange={(e) => setFormData({ ...formData, allowAnalytics: e.target.checked })}
                     disabled={!isEditing}
                     className="h-4 w-4 text-primary-600 focus:ring-primary-500 border-gray-300 dark:border-gray-600 rounded disabled:opacity-50"
